d2d696ad3f397799555bc5a563b86fc9ffb21f41
elizabethdggs <elizabeth.dominguez@cdmx.gob.mx>
2019-07-23 11:53:53 -0500

    reporte fotos

--- app/Exports/Reporteperiodo_miercolesExport.php
+++ app/Exports/Reporteperiodo_miercolesExport.php
@@ -48,7 +48,7 @@ class Reporteperiodo_miercolesExport implements WithDrawings, FromCollection, Wi
     {
         
 
-        return DB::table('tb_asistencias_miercoles')->select("tb_asistencias_miercoles.id","cat_delegaciones.delegacion","cat_coord_territorials.ct2","cat_coord_territorials.sector","tb_asistencias_miercoles.se_realizo","tb_asistencias_miercoles.no_motivo","tb_asistencias_miercoles.fecha","tb_asistencias_miercoles.hora_i","tb_asistencias_miercoles.hora_f","tb_asistencias_miercoles.jg","tb_asistencias_miercoles.mp","tb_asistencias_miercoles.jsp","tb_asistencias_miercoles.jspi","tb_asistencias_miercoles.jc","tb_asistencias_miercoles.ml",'tb_asistencias_miercoles.representante_alcaldia',"tb_asistencias_miercoles.otro","tb_asistencias_miercoles.ins","tb_asistencias_miercoles.vecino")
+        return DB::table('tb_asistencias_miercoles')->select("tb_asistencias_miercoles.id","cat_delegaciones.delegacion","cat_coord_territorials.ct2","cat_coord_territorials.sector","tb_asistencias_miercoles.se_realizo","tb_asistencias_miercoles.no_motivo","tb_asistencias_miercoles.fecha","tb_asistencias_miercoles.hora_i","tb_asistencias_miercoles.hora_f","tb_asistencias_miercoles.jg","tb_asistencias_miercoles.mp","tb_asistencias_miercoles.jsp","tb_asistencias_miercoles.jspi","tb_asistencias_miercoles.jc","tb_asistencias_miercoles.ml",'tb_asistencias_miercoles.representante_alcaldia',"tb_asistencias_miercoles.otro","tb_asistencias_miercoles.ins","tb_asistencias_miercoles.vecino","tb_asistencias_miercoles.archivo_imagen")
                        ->leftjoin('users','users.id','=','tb_asistencias_miercoles.user_registro') 
                       ->leftjoin('cat_coord_territorials','cat_coord_territorials.ct2','=','users.name')
                       ->leftjoin('cat_delegaciones','cat_delegaciones.id','=','cat_coord_territorials.id_alcaldia') 
@@ -80,7 +80,8 @@ public function headings(): array
             'REPRESENTANTE DE ALCALDÍA',
             'OTRO PARTICIPANTE',
             'PDI DE INTELIGENCIA SOCIAL', 
-            'NÚMERO DE VECINOS'
+            'NÚMERO DE VECINOS',
+            'CON IMAGEN'
 
             
         ];
