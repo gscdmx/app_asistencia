aa5c0179ac49bc8be6a29a6376915ed2f5a9b0ad
elizabethdggs <elizabeth.dominguez@cdmx.gob.mx>
2019-09-20 12:02:10 -0500

    cambios vistas4

--- resources/views/consulta_asistencia.blade.php
+++ resources/views/consulta_asistencia.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/r-2.2.2/sl-1.3.0/datatables.min.css"/>
-
 <style>
 thead {color:green;}
 tbody {color:blue;}
@@ -156,24 +154,6 @@ table, th, td {
 
 
 @section('customjs')
-
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/r-2.2.2/sl-1.3.0/datatables.min.js"></script>
-
-
-<script type="text/javascript">
-  
-
-  $(document).ready( function () {
-    $('#archivoslist').DataTable();
-} );
-
-</script>
-
-
-
-
-
-
 
 
 
