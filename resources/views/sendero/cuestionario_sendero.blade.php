a50741ba23f7165ca5fe3a338159c1e6fbf83ac3
elizadg <elizabeth.dominguez@cdmx.gob.mx>
2020-02-20 11:34:25 -0600

    cambios en hora vista sendero

--- resources/views/sendero/cuestionario_sendero.blade.php
+++ resources/views/sendero/cuestionario_sendero.blade.php
@@ -72,10 +72,11 @@
           
            <select name="hora_inicio" id="hora_inicio" class="form-control">
               <option value="">Selecciona Hora...</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+             
               
               
 
@@ -168,12 +169,15 @@
 
               <select name="hora_termino" id="hora_termino" class="form-control">
               <option value="">Selecciona Hora...</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
               <option value="4">4</option>
               <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
              
               </select>
             
@@ -473,7 +477,7 @@
         </div>
 
 
-<div class="line"></div>
+        <div class="line"></div>
 
 
           <div class="form-group row">
